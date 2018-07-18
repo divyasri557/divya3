4e76dd6038b9787ba30308ea2816e0dbe325a122
divyasri557 <41290520+divyasri557@users.noreply.github.com>
2018-07-18 16:20:23 +0530

    x  shape

---
+++ sample.java
@@ -0,0 +1,26 @@
+package srs;
+
+public class sample {
+public static void main(String[] args) {
+		int[] integer={1,2,3,2,1};
+		int length=integer.length;
+		for(int i=0;i<length;i++)
+		{
+			for(int j=0;j<length;j++)
+			{
+				if(j==i||j==length-1-i)
+				{
+					System.out.print(integer[j]);
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
